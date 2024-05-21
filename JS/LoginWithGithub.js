5c50d113d8b0e094ae2fbcaffed73e4e8c7cbfde
Circuit-Overtime <ayushbhatt633@gmail.com>
2024-05-21 19:41:05 +0530

    added credential checks

--- JS/LoginWithGithub.js
+++ JS/LoginWithGithub.js
@@ -1,4 +1,3 @@
-
 document.getElementById("signin_with_github").addEventListener("click", () => {
     var provider = new firebase.auth.GithubAuthProvider();
 
@@ -7,69 +6,72 @@ document.getElementById("signin_with_github").addEventListener("click", () => {
         var token = result.credential.accessToken;
         var user = result.user;
 
-    var docRef = db.collection("users").doc(user.displayName);
+        var docRef = db.collection("users").doc(user.displayName);
 
-
-      docRef.get().then((doc) => { //gets the whole data against the entered email address
-        if (doc.exists) 
-        {
+        docRef.get().then((doc) => { // gets the whole data against the entered email address
+            if (doc.exists) {
                 localStorage.setItem("CurUsername", user.displayName);
                 window.location.replace("s2.html");
-        }
-        else
-        {
-            document.getElementById("successLoaderContainer").classList.add("OpacityActive");
-            var TilesAnimationInterval = setInterval(() => {
-            var n = Math.floor(Math.random() * (numberOfTilesSpawned - 0 + 1)) + 0;
-            document.querySelectorAll(".tile")[n].click();
-            }, 1300 );
-
+            } else {
+                document.getElementById("successLoaderContainer").classList.add("OpacityActive");
+                var TilesAnimationInterval = setInterval(() => {
+                    var n = Math.floor(Math.random() * (numberOfTilesSpawned - 0 + 1)) + 0;
+                    document.querySelectorAll(".tile")[n].click();
+                }, 1300);
 
                 db.collection('users').doc(user.displayName.toLowerCase()).set({
-                    username : user.displayName.toLowerCase(),
-                    email : user.email,
-                    uid : user.uid,
-                    displayName : user.displayName,
+                    username: user.displayName.toLowerCase(),
+                    email: user.email,
+                    uid: user.uid,
+                    displayName: user.displayName,
                     dateCreated: date,
-                    isDev : "DEV",
+                    isDev: "DEV",
                     otp: 0,
                     provider: "Github",
                     lastPasswordChange: date,
                     user_logo: user.photoURL,
-                }).then(()=> {
+                }).then(() => {
                     var templateParams = {
                         name: user.displayName,
-                        service_name : "Github",
-                        email_provided : user.email,
+                        service_name: "Github",
+                        email_provided: user.email,
                     };
                     emailjs.send('service_lw32yzm', 'template_jqmgo3l', templateParams)
-                    .then(function(response) {
-                       console.log('SUCCESS!', response.status, response.text);
-                    }, function(error) {
-                        console.log('FAILED...', error);
-                        
-                    });
-                    
+                        .then(function(response) {
+                            console.log('SUCCESS!', response.status, response.text);
+                        }, function(error) {
+                            console.log('FAILED...', error);
+                        });
+
                     setTimeout(() => {
                         document.getElementById("successLoaderContainer").classList.remove("OpacityActive");
-                        
                         clearInterval(TilesAnimationInterval);
-                      }, 3300);
+                    }, 3300);
 
                     localStorage.setItem("CurUsername", user.displayName);
-                    window.location.replace("s2.html");       
+                    window.location.replace("s2.html");
                 })
                 .catch((err) => {
-                    console.log(err)
+                    console.error("Error adding document: ", err);
                     setTimeout(() => {
                         document.getElementById("successLoaderContainer").classList.remove("OpacityActive");
-                        
                         clearInterval(TilesAnimationInterval);
-                      }, 3300);
-                })
-
-                
+                    }, 3300);
+                });
+            }
+        }).catch((err) => {
+            console.error("Error getting document:", err);
+        });
+    })
+    .catch((err) => {
+        console.error("Error during signInWithPopup:", err.message);
+        if(err.message == "An account already exists with the same email address but different sign-in credentials. Sign in using a provider associated with this email address.")
+        {
+            RegisterError("Email Associated with Different Account/Service");
         }
-    })
-    })
+        else 
+        {
+            RegisterError("Some Error Occured!");
+        }
+    });
 });
--- JS/RegisterWithGoogle.js
+++ JS/RegisterWithGoogle.js
@@ -1,39 +1,34 @@
-
 document.getElementById("signin_with_google_reg").addEventListener("click", () => {
     var provider = new firebase.auth.GoogleAuthProvider();
 
-  firebase.auth().signInWithPopup(provider)
+    firebase.auth().signInWithPopup(provider)
     .then(function(result) {
-    
-      // This gives you a Google Access Token.
-      var token = result.credential.accessToken;
-      console.log(token)
-      // This gives you the signed-in user.
-      var user = result.user;
-      console.log(user);
-      var docRef = db.collection("users").doc(user.displayName);
-      docRef.get().then((doc) => { //gets the whole data against the entered email address
-        if (doc.exists) 
-        {
+        // This gives you a Google Access Token.
+        var token = result.credential.accessToken;
+        console.log(token);
+        // This gives you the signed-in user.
+        var user = result.user;
+        console.log(user);
+        var docRef = db.collection("users").doc(user.displayName);
+        docRef.get().then((doc) => { // gets the whole data against the entered email address
+            if (doc.exists) {
                 localStorage.setItem("CurUsername", user.displayName);
                 window.location.replace("s2.html");
-        }
-        else
-        {
-          document.getElementById("successLoaderContainer").classList.add("OpacityActive");
-            var TilesAnimationInterval = setInterval(() => {
-            var n = Math.floor(Math.random() * (numberOfTilesSpawned - 0 + 1)) + 0;
-            document.querySelectorAll(".tile")[n].click();
-            }, 1300 );
+            } else {
+                document.getElementById("successLoaderContainer").classList.add("OpacityActive");
+                var TilesAnimationInterval = setInterval(() => {
+                    var n = Math.floor(Math.random() * (numberOfTilesSpawned - 0 + 1)) + 0;
+                    document.querySelectorAll(".tile")[n].click();
+                }, 1300);
 
-                    db.collection('users').doc(user.displayName.toLowerCase()).set({
-                    username : user.displayName.toLowerCase(),
-                    email : user.email,
-                    uid : user.uid,
-                    emailVerified : user.emailVerified,
-                    displayName : user.displayName,
+                db.collection('users').doc(user.displayName.toLowerCase()).set({
+                    username: user.displayName.toLowerCase(),
+                    email: user.email,
+                    uid: user.uid,
+                    emailVerified: user.emailVerified,
+                    displayName: user.displayName,
                     dateCreated: date,
-                    isDev : "DEV",
+                    isDev: "DEV",
                     otp: 0,
                     provider: "Google",
                     lastPasswordChange: date,
@@ -41,58 +36,38 @@ document.getElementById("signin_with_google_reg").addEventListener("click", () =
                 }).then(() => {
                     var templateParams = {
                         name: user.displayName,
-                        service_name : "Google",
-                        email_provided : user.email,
+                        service_name: "Google",
+                        email_provided: user.email,
                     };
                     emailjs.send('service_lw32yzm', 'template_jqmgo3l', templateParams)
-                    .then(function(response) {
-                       console.log('SUCCESS!', response.status, response.text);
-                    }, function(error) {
-                       console.log('FAILED...', error);
-                    });
-                    
+                        .then(function(response) {
+                            console.log('SUCCESS!', response.status, response.text);
+                        }, function(error) {
+                            console.log('FAILED...', error);
+                        });
+
                     setTimeout(() => {
-                      document.getElementById("successLoaderContainer").classList.remove("OpacityActive");
-                      
-                      clearInterval(TilesAnimationInterval);
+                        document.getElementById("successLoaderContainer").classList.remove("OpacityActive");
+                        clearInterval(TilesAnimationInterval);
                     }, 3300);
 
-                    
                     localStorage.setItem("CurUsername", user.displayName);
                     window.location.replace("s2.html");
                 }).catch((err) => {
-                    console.log(err);
-                })
-
-           
-        }
-    })
-      
+                    console.error("Error adding document:", err);
+                });
+            }
+        });
     })
     .catch(function(error) {
-      // Handle errors here.
-      var errorCode = error.code;
-      var errorMessage = error.message;
-      console.error("Google Sign-In Error:", errorCode, errorMessage);
+        // Handle errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        if(error.message)
+        {
+            LoginError("There's an Error");
+        }
     });
-})
+});
 
 
-document.getElementById("signin_with_google").addEventListener("click", () => {
-    
-    // emailjs.send("service_lw32yzm", "template_jqmgo3l", templateParams, {
-    //     SecureToken: "service_lw32yzm",
-    //     To: "ayushbhatt633@gmail.com",
-    //     From: "safe.cypher@gmail.com",
-    //     Subject: "This is the subject",
-    //     Body: "body content",
-    //     TemplateID: "template_jqmgo3l"
-    //   }).then(function(response)
-    //   {
-    //     message => alert(message)
-    //   }); 
-
-
-})
-
-// template_37zjnlx
